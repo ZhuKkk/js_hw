6e3e7f5d8171c409d650f282764f68acf61c33f8
Raman <48173416+ZhuKkk@users.noreply.github.com>
2019-03-28 15:26:50 +0300

    Delete №1.js

--- №1.js
+++
@@ -1,9 +0,0 @@
-function myCounter() {
-    var curCount = 1;
-    return function(){
-        return curCount++;};
-}
-var counter = myCounter();
-alert( counter() );
-alert( counter() );
-alert( counter() );
